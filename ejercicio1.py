28c4da023911e0d8ec42ce4df360daf1881df606
Carloscp92 <carmor.cpc92@gmail.com>
2025-12-23 11:30:40 +0100

    subida ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+"""
+Escribe un programa en Python que pida al usuario su nombre y edad,
+ y luego imprima un mensaje de bienvenida customizado indicando ambos datos.
+"""
+
+nombre = input("Introduce tu nombre:")
+edad= int(input("introduce tu edad:"))
+
+print(f"Hola {nombre} te ves muy joven para tener {edad} años")
